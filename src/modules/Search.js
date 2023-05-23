547e5345cadb6bd1b046bf0e77b251e48e837808
VitoMao <vito_mao@hotmail.com>
2023-05-23 15:30:26 +0800

    Load WP Content with JS

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -45,7 +45,13 @@ class Search {
   }
 
   getResults() {
-    this.resultsDiv.html("Imagine real search results.");
+    $.getJSON(
+      "http://fictional-university.local/wp-json/wp/v2/posts?search=" +
+        this.searchField.val(),
+      function (posts) {
+        alert(posts[0].title.rendered);
+      }
+    );
     this.isSpinnerVisible = false;
   }
 
